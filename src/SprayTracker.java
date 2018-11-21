4eff75c67b58818082107504fd40f6bf1b6b8b1b
erik ben heckman <erik@heckman.ca>
2018-11-20 16:01:51 -0800

    Merge-in and bug-fix Mo's SprayTracker class.

--- src/SprayTracker.java
+++ src/SprayTracker.java
@@ -1,31 +1,92 @@
+import java.util.Random;
+
 public class SprayTracker implements TrackerInterface {
+  private static final int MIN_SPRAY_LEVEL = 10;
+  private static final int SPRAY_AMOUNT = 15;
+  private static final int MAX_SPRAY_LEVEL = 100;
+  private static final int MAX_MULTIPLIER = 2;
+  private static final int BUGGY_STILL_HEALTHY = 10;
+  private static final int DEAD_FROM_BUGS = 100;
+  private static final double PROBABILITY_OF_NEW_BUGS = 0.10;
+  private static final double PROBABILITY_OF_MORE_BUGS = 0.15;
+
+  private int sprayLevel;
+  private double numberOfBugs;
+
+  private Random random = new Random();
+
+  SprayTracker() {
+    numberOfBugs = 0;
+    sprayLevel = 0;
+  }
+
+  private boolean hasBugs() {
+    return numberOfBugs > 0;
+  }
+
+  // decrement spray-level every day,
+  // calls bugs if spray level is low
   @Override
   public void step() {
-
+    sprayLevel -= 1;
+    if ( sprayLevel < 0 )
+      sprayLevel = 0;
+    if ( !hasBugs() ) {
+      if ( sprayLevel < MIN_SPRAY_LEVEL )
+        bugCaller();
+    } else {
+      bugMultiplier();
+    }
   }
 
+  // apply pesticides
   @Override
   public void apply() {
-
+    sprayLevel += SPRAY_AMOUNT;
+    if ( sprayLevel > MAX_SPRAY_LEVEL )
+      sprayLevel = MAX_SPRAY_LEVEL;
+    // kill bugs
+    bugKiller();
   }
 
+  // return status string
   @Override
   public String getStatus() {
-    return null;
+    return hasBugs() ? "It has bugs. " : null;
+  }
+
+  // if spray level is below a certain point bugs might appear
+  private void bugCaller() {
+    if ( random.nextDouble() < PROBABILITY_OF_NEW_BUGS )
+      numberOfBugs = 1; // will multiply quickly
+  }
+
+  // should only be called weekly, once a week? - put in random chance
+  private void bugMultiplier() {
+    if ( random.nextDouble() < PROBABILITY_OF_MORE_BUGS )
+      numberOfBugs *= (random.nextDouble() * MAX_MULTIPLIER + 1);
+  }
+
+  // kill some bugs (if there are any)
+  private void bugKiller() {
+    if ( hasBugs() )
+      numberOfBugs *= random.nextDouble();
   }
 
   @Override
   public boolean isHealthy() {
-    return false;
+    return numberOfBugs < BUGGY_STILL_HEALTHY;
   }
 
+  // if bugs go over a certain number, plant is dead
   @Override
   public boolean isDead() {
-    return false;
+    return numberOfBugs > DEAD_FROM_BUGS;
   }
 
+  // return reason plant died: from bugs
   @Override
   public String getCauseOfDeath() {
-    return null;
+    return "The plant was eaten up by bugs.";
   }
 }
