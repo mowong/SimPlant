ceab3c61c3f6352fbd9cde199bddc8d058417e7b
erik ben heckman <erik@heckman.ca>
2018-11-23 14:44:19 -0800

    Refactor SprayTracker to extend Tracker class

--- src/SprayTracker.java
+++ src/SprayTracker.java
@@ -1,38 +1,81 @@
 import java.util.Random;
 
-public class SprayTracker implements TrackerInterface {
+public class SprayTracker extends Tracker {
+
+  private static final Tracker.Levels LEVELS = new Tracker.Levels(
+      0.0,
+      0.0, 0.0, // not used
+      0.0, 0.0  // not used
+  );
+
+  private static final Tracker.Zone[] ZONES =
+      {
+          // must be in order from largest to smallest
+          // minimum number, healthy, deadly, array of status messages
+          new Tracker.Zone("+X", 100.0, false, true,
+                           new String[]{
+                               "It has turned into a plant skelton. ",
+                               "The bugs ate it all up. "
+                           }
+          ),
+          new Tracker.Zone("+4", 80.0, false, false,
+                           new String[]{
+                               "The bugs are sending scouting parties to find new plants to eat. ",
+                               "There's not much plant left. "
+                           }
+          ),
+          new Tracker.Zone("+3", 65.0, false, false,
+                           new String[]{
+                               "The bugs are starting to take over. ",
+                               "The bugs are making themselves at home. "
+                           }
+          ),
+          new Tracker.Zone("+2", 30.0, false, false,
+                           new String[]{
+                               "It's getting a buggy. "
+                           }
+          ),
+          new Tracker.Zone("+1", 10, true, false,
+                           new String[]{
+                               "What's a few bugs between friends? "
+                           }
+          ),
+          new Tracker.Zone("+0", -1.0, true, false,
+                           new String[]{
+                               "No bugs to be seen. "
+                           }
+          )
+      };
+
   private static final int MIN_SPRAY_LEVEL = 10;
-  private static final int SPRAY_AMOUNT = 15;
-  private static final int MAX_SPRAY_LEVEL = 100;
+  private static final int MAX_SPRAY_LEVEL = 50;
+  private static final int SPRAY_AMOUNT = 10;
+  private static final int STARTING_SPRAY_AMOUNT = 20;
   private static final int MAX_MULTIPLIER = 2;
-  private static final int BUGGY_STILL_HEALTHY = 10;
-  private static final int DEAD_FROM_BUGS = 100;
-  private static final float PROBABILITY_OF_NEW_BUGS = 0.10F;
-  private static final float PROBABILITY_OF_MORE_BUGS = 0.15F;
+  private static final double PROBABILITY_OF_NEW_BUGS = 0.10;
+  private static final double PROBABILITY_OF_MORE_BUGS = 0.15;
 
-  private int sprayLevel;
-  private float numberOfBugs;
-
+  private int spray;
   private Random random = new Random();
 
   SprayTracker() {
-    numberOfBugs = 0;
-    sprayLevel = 0;
+    super(LEVELS, ZONES);
+    spray = STARTING_SPRAY_AMOUNT;
   }
 
   private boolean hasBugs() {
-    return numberOfBugs > 0;
+    return getLevel() > 0;
   }
 
   // decrement spray-level every day,
   // calls bugs if spray level is low
   @Override
   public void step() {
-    sprayLevel -= 1;
-    if ( sprayLevel < 0 )
-      sprayLevel = 0;
+    spray -= 2;
+    if ( spray < 0 )
+      spray = 0;
     if ( !hasBugs() ) {
-      if ( sprayLevel < MIN_SPRAY_LEVEL )
+      if ( spray < MIN_SPRAY_LEVEL )
         bugCaller();
     } else {
       bugMultiplier();
@@ -42,57 +85,41 @@ public class SprayTracker implements TrackerInterface {
   // apply pesticides
   @Override
   public void apply() {
-    sprayLevel += SPRAY_AMOUNT;
-    if ( sprayLevel > MAX_SPRAY_LEVEL )
-      sprayLevel = MAX_SPRAY_LEVEL;
+    spray += SPRAY_AMOUNT;
+    if ( spray > MAX_SPRAY_LEVEL )
+      spray = MAX_SPRAY_LEVEL;
     // kill bugs
     bugKiller();
   }
 
-  // return status string
-  @Override
-  public String getStatus() {
-    return hasBugs() ? "It has bugs. " : null;
-  }
-
   // if spray level is below a certain point bugs might appear
   private void bugCaller() {
-    if ( random.nextFloat() < PROBABILITY_OF_NEW_BUGS )
-      numberOfBugs = 1; // will multiply quickly
+    if ( random.nextDouble() < PROBABILITY_OF_NEW_BUGS )
+      setLevel(1.0); // will multiply quickly
   }
 
   // should only be called weekly, once a week? - put in random chance
   private void bugMultiplier() {
-    if ( random.nextFloat() < PROBABILITY_OF_MORE_BUGS )
-      numberOfBugs *= (random.nextFloat() * MAX_MULTIPLIER + 1);
+    if ( random.nextDouble() < PROBABILITY_OF_MORE_BUGS )
+      setLevel(getLevel() * (random.nextDouble() * MAX_MULTIPLIER + 1));
   }
 
   // kill some bugs (if there are any)
   private void bugKiller() {
     if ( hasBugs() )
-      numberOfBugs *= random.nextFloat();
+      setLevel(getLevel() * random.nextDouble()); // maybe add a multiplier constant?
+    if ( getLevel() < 1 )
+      setLevel(0);
   }
 
   @Override
-  public boolean isHealthy() {
-    return numberOfBugs < BUGGY_STILL_HEALTHY;
-  }
-
-  // if bugs go over a certain number, plant is dead
-  @Override
-  public boolean isDead() {
-    return numberOfBugs > DEAD_FROM_BUGS;
-  }
-
-  // return reason plant died: from bugs
-  @Override
-  public String getCauseOfDeath() {
-    return "The plant was eaten up by bugs.";
+  public String toString() {
+    return super.toString("Bug ") + String.format(", Spray level: %02d", spray);
   }
 
   @Override
   public String getLevelCode() {
-    return String.format("s%02d,p%02.0f",sprayLevel,numberOfBugs);
+    return super.getLevelCode("p") + String.format("s%02d", spray);
   }
 
 }
