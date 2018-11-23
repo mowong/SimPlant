b2f1669926e4ac94763f9a1188f2dadc0800d59b
erik ben heckman <erik@heckman.ca>
2018-11-23 13:56:23 -0800

    Feed and Water Trackers extend new Tracker class

--- src/WaterTracker.java
+++ src/WaterTracker.java
@@ -1,7 +1,4 @@
-import java.util.Random;
-import java.util.stream.Stream;
-
-public class WaterTracker implements TrackerInterface {
+class WaterTracker extends Tracker {
 
   // LEVEL LOGIC
   // One application adds the same amount as 8 days removes.
@@ -9,153 +6,92 @@ public class WaterTracker implements TrackerInterface {
   // it wall die after 3 applications at once and
   // it will die after 16 days without an application.
 
-  private static final double IDEAL_LEVEL = 40.0;
+  private static final Tracker.Levels LEVELS = new Tracker.Levels(
+      40.0,
+      2.5, 8.0,
+      20, 8.0
+  );
 
-  private static final double DECREMENT = 2.5;
-  // 68% of the time it will be ± this amount:
-  private static final double DECREMENT_SD = 0.125; // about 8%
+  private static final Tracker.Zone[] ZONES =
+      {
+          // IDEAL + 4 applications  (minimum for zone)
+          new Tracker.Zone(
+              "+X", 100.0, false, true,
+              new String[]{
+                  "It has drowned. ",
+                  "Your plant got so wet it developed a gnarly " +
+                  "fungus that took over everything. ",
+                  "There is so much water it suffocated. "
+              }
+          ),
+          // IDEAL + 2 applications  (minimum for zone)
+          new Tracker.Zone(
+              "+2", 80.0, false, false,
+              new String[]{
+                  "The leaves are drooping quite badly. ",
+                  "It's wilting quite a bit. "
+              }
+          ),
+          // IDEAL + 1.25 applications  (minimum for zone)
+          new Tracker.Zone(
+              "+1", 65.0, false, false,
+              new String[]
 
+                  {
+                      "The leaves are a little bit limp. ",
+                      "It looks a little sad. "
+                  }
+          ),
+          // IDEAL ZONE
+          new Tracker.Zone(
+          "±0",30.0, true, false,
+                           new String[]
 
-  private static final double INCREMENT = 20; //
-  // 68% of the time it will be ± this amount:
-  private static final double INCREMENT_SD = 1; // 8%
+                               {
+                                   "The leaves are perfectly perky. "
+                               }
+          ),
+          // IDEAL + 8 days  (maximum for zone)
+          new Tracker.Zone(
+              "-1",10.0, false, false,
+                           new String[]
 
-  private enum Zone {
-    // must be in order from largest to smallest
-    // minimum percentage, healthy, deadly, array of status messages
+                               {
+                                   "The leaves are a little bit limp. "
+                               }
+          ),
+          // IDEAL + 12 days  (maximum for zone)
+          new Tracker.Zone(
+              "-2",0.0, false, false,
+                           new String[]
 
-    // IDEAL + 4 applications
-    MORE_XX(100.0, false, true,
-            new String[]{
-                "It has drowned. ",
-                "Your plant got so wet it developed a gnarly " +
-                "fungus that took over everything. ",
-                "There is so much water it suffocated. "
-            }
-    ),
-    // IDEAL + 2.8 applications
-    MORE_02(80.0, false, false,
-            new String[]{
-                "The leaves are drooping quite badly. ",
-                "It's wilting quite a bit. "
-            }
-    ),
-    // IDEAL + 1.6 applications
-    MORE_01(65.0, false, false,
-            new String[]{
-                "The leaves are a little bit limp. ",
-                "It looks a little sad. "
-            }
-    ),
-    // IDEAL + 8 days
-    PERFECT(30.0, true, false,
-            new String[]{
-                "The leaves are perfectly perky. "
-            }
-    ),
-    // IDEAL + 12 days
-    LESS_01(10.0, false, false,
-            new String[]{
-                "The leaves are a little bit limp. "
-            }
-    ),
-    // not quite dead
-    LESS_02(0.0, false, false,
-            new String[]{
-                "The leaves are drooping quite badly. "
-            }
-    ),
-    // IDEAL +
-    LESS_XXX(-1.0, false, true,
-             new String[]{
-                 "There is nothing left of it but a dried-out husk. "
-             }
-    );
+                               {
+                                   "The leaves are drooping quite badly. "
+                               }
+          ),
+          // IDEAL + 16 days  (maximum for zone)
+          new Tracker.Zone(
+              "-X",-1.0, false, true,
+                           new String[]
 
-    private double min;
-    private boolean healthy;
-    private boolean deadly;
-    private String[] statuses;
-
-    public double getMin() {
-      return min;
-    }
-
-    Zone(double min, boolean healthy, boolean deadly, String[] statuses) {
-      this.min = min;
-      this.healthy = healthy;
-      this.deadly = deadly;
-      this.statuses = statuses;
-    }
-
-    static Zone getZone(double percentage) {
-      return Stream
-                 .of(Zone.values())
-                 .filter(g -> percentage > g.getMin())
-                 .findFirst()
-                 .orElseThrow(() -> new IllegalStateException("No valid Zone."));
-    }
-
-    public boolean isHealthy() {
-      return healthy;
-    }
-
-    public boolean isDeadly() {
-      return deadly;
-    }
-
-    public String getStatus() {
-      return statuses[new Random().nextInt(statuses.length)];
-    }
-  }
-
-  private Random random = new Random();
-  private double level; // percentage
+                               {
+                                   "There is nothing left of it but a dried-out husk. "
+                               }
+          )
+      };
 
   WaterTracker() {
-    level = IDEAL_LEVEL;
-  }
-
-  @Override
-  public void step() {
-    level -= DECREMENT + DECREMENT_SD * random.nextGaussian();
-    if ( level < 0 ) level = 0; // > -1 and <= 0 is dead
-  }
-
-  @Override
-  public void apply() {
-    level += INCREMENT + INCREMENT_SD * random.nextGaussian();
-    if ( level > 100 ) level = 101; // > 100 is dead
-  }
-
-  @Override
-  public String getStatus() {
-    return Zone.getZone(level).getStatus();
-  }
-
-  @Override
-  public boolean isHealthy() {
-    return Zone.getZone(level).isHealthy();
-  }
-
-  @Override
-  public boolean isDead() {
-    return Zone.getZone(level).isDeadly();
-  }
-
-  @Override
-  public String getCauseOfDeath() {
-    return Zone.getZone(level).getStatus();
+    super(LEVELS, ZONES);
   }
 
   @Override
   public String toString() {
-    return String.format("Water level: %.2f",level);
+    return super.toString("Water ");
   }
 
   @Override
   public String getLevelCode() {
-    return String.format("w%02.0f",level);
+    return super.getLevelCode("w");
   }
 
 }
