5382211981be1ece93712c0083fb8231b12d694f
erik ben heckman <erik@heckman.ca>
2018-11-24 17:27:40 -0800

    Reorganize utils, add launcher, make shells scripts executable
    Also make a local copy of jquery library

---
+++ utils/deploy_web-interface.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+#  copy web interface to mnrva
+#
+#
+scp server/web-interface/* mnrva:177/app/web-interface
---
+++ utils/mnrva/simplant-daemon.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+#  run simplant (must be in current directory)
+#
+#
+echo | java -jar ../out/artifaces/simplant_jar/simplant.jar &
