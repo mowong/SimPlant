984dec6e15b88e786ee16350d103ecd29e48b723
erik ben heckman <erik@heckman.ca>
2018-12-05 18:29:33 -0800

    Game state revisions & less-awkward text between games.

--- src/Game.java
+++ src/Game.java
@@ -5,13 +5,21 @@ class Game {
   private String id;
   private Plant plant;
   private GameLoader loader;
-  private GameState gameState;
-  private GameState lastState;
+  private State state;
   private int oldestPlant;
 
+  private enum State {
+    IN_GAME,
+    CONFIRM_QUIT,
+    BETWEEN_GAMES,
+    CONFIRM_NEW
+  }
+
+  private static final int NO_PLANTS_TO_AGE = -1;
+
   Game(GameLoader loader, String id) {
-    gameState = GameState.NEW_PLAYER;
-    oldestPlant = -1;
+    state = State.BETWEEN_GAMES;
+    oldestPlant = NO_PLANTS_TO_AGE;
     //// in a future version Game will call loader with id to delete a player
     // this.id = id;
     // this.loader = loader;
@@ -20,33 +28,30 @@ class Game {
   // all threads enter through here
   // therefore this is the only method that needs to be synchronized
   synchronized String processCommand(String rawCommand) {
-    //String command = (rawCommand + "   ").substring(0, 3).toLowerCase();
     String command = rawCommand.toLowerCase();
     try {
-      switch ( gameState ) {
-        case NEW_PLAYER:
-          return newPlayer();
-        case GAME_IS_ON:
-          return gameIsOn(command);
-        case GAME_OVER:
-          return gameOver();
+      switch ( state ) {
+        case BETWEEN_GAMES:
+          return betweenGames();
+        case IN_GAME:
+          return gameCommand(command);
         case CONFIRM_NEW:
           return yesOrNo(command) ? confirmNew() : abortNew();
         case CONFIRM_QUIT:
           return yesOrNo(command) ? confirmQuit() : abortQuit();
       }
     } catch ( IllegalArgumentException e ) {
-      switch ( gameState ) {
+      switch ( state ) {
         case CONFIRM_NEW:
           return "Please answer 'yes' or 'no': " + askNew();
         case CONFIRM_QUIT:
           return "Please answer 'yes' or 'no': " + askQuit();
-        case GAME_IS_ON:
+        case IN_GAME:
           return helpMessage();
+        // BETWEEN_GAMES: doesn't throw exceptions
       }
     }
-
-    return null;
+    throw new IllegalStateException();
   }
 
   private boolean yesOrNo(String command)
@@ -72,16 +77,17 @@ class Game {
     }
   }
 
-  private void revertState() {
-    gameState = lastState;
+
+  private void setState(State newState) {
+    // used to do more here like keep track of previous state
+    state = newState;
   }
 
-  private void setState(GameState newState) {
-    lastState = gameState;
-    gameState = newState;
+  private boolean isNewPlayer() {
+    return oldestPlant == NO_PLANTS_TO_AGE;
   }
 
-  private String gameIsOn(String command)
+  private String gameCommand(String command)
       throws IllegalArgumentException {
     switch ( command ) {
 
@@ -132,85 +138,90 @@ class Game {
     }
   }
 
+  private String helpMessage() {
+    return "You can \'water\', \'feed', \'spray\' or \'check\' your plant. \n " +
+           "You can also \'kill\' your plant to start again.";
+  }
+
   private String doAction(PlantAction action) {
     return checkForDead(plant.action(action));
   }
 
   private String checkForDead(String message) {
     if ( plant.isDead() ) {
-      updateOldestPlant();
-      setState(GameState.GAME_OVER);
-      return message + gameOver();
+      // end current game... STATE: IN_GAME -> CONFIRM_NEW
+      return message + endGame();
+    } else {
+      return message;
     }
-    return message;
   }
 
-  private void updateOldestPlant() {
+  // STATE:  IN_GAME or CONFIRM_QUIT -> CONFIRM_NEW
+  private String endGame() {
     int age = plant.getAge();
-    if ( age > oldestPlant ) oldestPlant = age;
-  }
-
-  private String helpMessage() {
-    return "You can \'water\', \'feed', \'spray\' or \'check\' your plant. \n " +
-           "You can also \'kill\' your plant to start again.";
-  }
-
-  private String getHowToGetHelp() {
-    return "(Send 'help' for help!)";
-  }
-
-  // asks to confirm starting a new plant (new players)
-  private String newPlayer() {
-    return "Welcome to SimPlant! " + askNew();
-  }
-
-  // asks to confirm starting a new plant (returning players)
-  private String gameOver() {
-    return "Your oldest plant was " + Plant.ageString(oldestPlant) + ". "
+    if ( age < oldestPlant )
+      return "Your oldest plant was "
+             + Plant.ageString(oldestPlant)
+             + askNew();
+    //  age >= oldestPlant
+    oldestPlant = age;
+    return "Your oldest so far! "
            + askNew();
   }
 
-
-  // asks to confirm starting a new plant
-  private String askNew() {
-    setState(GameState.CONFIRM_NEW);
-    return "Would you like to grow a new plant?";
+  // STATE:  NEW_PLAYER -> CONFIRM NEW
+  private String betweenGames() {
+    return (isNewPlayer()
+                ? "Welcome to SimPlant! "
+                : "Welcome back to Simplant! Your oldest plant was "
+                  + Plant.ageString(oldestPlant) + ". "
+           ) + askNew();
   }
 
-  // starts a new plant
+  // STARTING A NEW GAME
+
+  // STATE:  NEW_PLAYER or BETWEEN_GAMES -> CONFIRM_NEW
+  private String askNew() {
+    setState(State.CONFIRM_NEW);
+    return "Would you like to grow a "
+           + (isNewPlayer() ? "plant?" : "new plant?");
+  }
+
+  // STATE:  CONFIRM_NEW -> IN_GAME
   private String confirmNew() {
-    setState(GameState.GAME_IS_ON);
+    setState(State.IN_GAME);
     plant = new Plant();
     return "You have a new plant! " +
            plant.action(PlantAction.CHECK) +
            " " + // action status strips trailing space
-           getHowToGetHelp();
+           "(Send 'help' for help!)";
   }
 
-  // returns to previous state
+  // STATE:  CONFIRM_NEW -> BETWEEN_GAMES
   private String abortNew() {
-    setState(GameState.GAME_OVER); // revertState();
+    setState(State.BETWEEN_GAMES);
     return "Let me know when you change your mind! (Any text will do.)";
   }
 
-  // asks to confirm quitting a plant
+  // QUITTING A GAME
+
+  // STATE:  IN_GAME -> CONFIRM_QUIT
   private String askQuit() {
-    setState(GameState.CONFIRM_QUIT);
+    setState(State.CONFIRM_QUIT);
     return "Are you sure you want to quit the game? " +
            "(Your plant will be lost!)";
   }
 
-  // returns to previous state
+  // STATE:  CONFIRM_QUIT -> IN_GAME
   private String abortQuit() {
-    setState(GameState.GAME_IS_ON); // revertState();
+    setState(State.IN_GAME);
     return "Ok good. " + doAction(PlantAction.CHECK);
   }
 
-  // quits the game
+  // STATE:  CONFIRM_QUIT -> CONFIRM_NEW
   private String confirmQuit() {
-    updateOldestPlant();
-    setState(GameState.GAME_OVER);
-    return gameOver();
+    return "Your plant was " + Plant.ageString(plant.getAge()) + ". "
+           + endGame();
   }
 
 
